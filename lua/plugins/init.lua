5adf4ed6a48f5c2f2aaf0e5eb9df490217bf2424
LeaFZ <leafzhang714@gmail.com>
2023-01-17 15:16:10 -0500

    Add coc

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,11 +17,6 @@ return {
     },
   },
 
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  },
-
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = function()
